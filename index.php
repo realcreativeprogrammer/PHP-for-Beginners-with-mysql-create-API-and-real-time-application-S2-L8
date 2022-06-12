11c8b18c8a5ecfabd193130fc04fb64d6e1188f2
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2022-06-12 13:54:33 +0300

    first

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo date('Y:m:d');
+echo '<br>';
+echo date('Y');
+echo '<br>';
+echo date('m');
+echo '<br>';
+echo date('d');
+echo '<br>';
+echo date('Y/m/d');
+echo '<br>';
+echo date('h:i:sa');
+echo '<br>';
+echo date('h');
+echo '<br>';
+echo date('Y/m/d : h:i:sa');
+echo '<br>';
+?>
